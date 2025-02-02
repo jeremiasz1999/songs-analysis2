5d897a708149c12bd03b0c803d006cd9b26dcbfb
marta <marta.klebba123@gmail.com>
2025-02-02 14:02:53 +0100

    dodanie końcowej wersji modelu uczenia maszynowego

--- Uczenie maszynowe.py
+++ Uczenie maszynowe.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 #wczytywanie data setu
@@ -53,15 +55,29 @@ else:
     # kategorycznych, więc kodujemy 'track_genre'
     from sklearn.preprocessing import LabelEncoder
 
-df.head(20)
+    le = LabelEncoder()
+    X_train['genre'] = le.fit_transform(X_train['genre'])
+    X_test['genre'] = le.transform(X_test['genre'])  # Używamy tylko `transform` dla danych testowych
+    # df.head(20)
+    # podział danych i trenowanie model
 
-#podział danych i trenowanie modelu
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+    # podział danych i trenowanie modelu
+    from sklearn.model_selection import train_test_split
+    from sklearn.ensemble import RandomForestRegressor
 
-#oddzielenie cech (x) od zmiennej docelowej (y)
-X = df.drop('popularity', axis=1) #cechy: wszytskie kolumny poza popularity
-y = df['popularity'] #zmienna docelowa: popularity
+    # oddzielenie cech (x) od zmiennej docelowej (y)
+    X = df.drop('popularity', axis=1)  # cechy: wszytskie kolumny poza popularity
+    y = df['popularity']  # zmienna docelowa: popularity
 
-#podział na dane treningowe i testowe
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # podział na dane treningowe i testowe
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # inicjalizacaj modelu Random Forest
+    rf = RandomForestRegressor(n_estimators=100, random_state=42)
+    # trenowanie modelu na danych treningowych
+    rf.fit(X_train, y_train)
+
+    # przewidywanie wyników danych testowych
+    y_pred = rf.predict(X_test)
+    print(type(y_test))
+    print(y_test[:5])  # Podgląd pierwszych 5 wartości
