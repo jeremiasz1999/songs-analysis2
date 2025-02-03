b451b2f050090d2b3cd9b220e10ce840d17c7781
Piotr-Przypasniak <p.przypasniak.550@studms.ug.edu.pl>
2025-02-03 14:39:30 +0100

    Cleaned data, added plots and added Metadata.txt

---
+++ Wykresy.py
@@ -0,0 +1,30 @@
+from song_popularity_analysis import df
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+
+# Dividing parameters into numerical and categorical
+cat = df[["key","mode", "time_signature", "key_mode"]].copy()
+num = df.drop(["key","mode", "time_signature"], axis = 1)
+
+# ///////////////////////////////// Wykresy zmiennych ciągłych \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+# Tworzymy wykres o rozmiarze (30, 30) i jedną oś (ax)
+fig, ax = plt.subplots(figsize=(30, 30))
+num.hist(ax=ax)
+plt.show()
+
+
+# ////////////////////////////// Wykresy zmiennych kategorycznych \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+key_mode_counts = df['key_mode'].value_counts()
+colors = ['red' if key.isupper() else 'blue' for key in key_mode_counts.index]
+
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize = (15,5))
+#-------------------------------------------------------------------------------------------------
+cat["key_mode"].value_counts().plot(kind='bar', ax = ax1, title = "Key Mode", color=colors)
+moll_patch = mpatches.Patch(color='blue', label='moll (minor)')
+dur_patch = mpatches.Patch(color='red', label='dur (major)')
+ax1.legend(handles=[moll_patch, dur_patch], loc='upper right')
+#-------------------------------------------------------------------------------------------------
+cat["time_signature"].value_counts().plot(kind='bar', ax = ax2, title = "Time signature")
+cat["mode"].value_counts().plot(kind='bar', ax = ax3, title = "audio mode")
+plt.show()
--- song_popularity_analysis.py
+++ song_popularity_analysis.py
@@ -1,8 +1,64 @@
 import numpy as np
 import pandas as pd
+import os
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import seaborn as sns
+from collections import Counter, OrderedDict
+from IPython.display import Markdown, display
 
 #Wprowadzenie data setu do programu
 csv_file = "ostateczny_dataset.csv"
 df = pd.read_csv(csv_file)
 df = df.drop('track_id',axis = 1)
 df = df.drop('Unnamed: 0',axis = 1)
+
+# Wyświetlanie innformacji o datasecie
+#df.info()
+
+# Removing duplicated
+counter = Counter(df['track_name'])
+if len(Counter({k: c for k, c in counter.items() if c > 1})) != 0:
+  df = df.drop_duplicates(subset='track_name')
+  df = df.reset_index(drop=True)
+
+
+# Removing songs longer than 10 min. 
+# They are likely not a single song, but a compilation of many
+df = df.loc[df["duration_ms"] < 10*60000]
+
+
+# Removing songs with a large value of speechiness
+# They are likely a podcast or a recitation
+df = df.loc[df["speechiness"] < 0.66]
+
+
+# Removing songs with assigned time signature of 0 and 1
+# Music theory does not allow a time signature of 0; this is likely a mistake by the dataset authors
+# While a time signature of 1 is theoretically possible, it is very unlikely for this value to be intentional
+df.drop(df[ df['time_signature'] == 1 ].index, inplace = True)
+df.drop(df[ df['time_signature'] == 0 ].index, inplace = True)
+Counter(df['time_signature'])
+
+
+# For clearer presentation, values of the 'key' parameter {0, 1, ..., 11} should be replaced with the corresponding musical note names
+key_mapping = {
+    0: 'C', 1: 'C♯/D♭', 2: 'D', 3: 'D♯/E♭', 4: 'E', 5: 'F',
+    6: 'F♯/G♭', 7: 'G', 8: 'G♯/A♭', 9: 'A', 10: 'A♯/B', 11: 'H'
+}
+df['key'] = df['key'].replace(key_mapping)
+
+
+# It's useful to analyze musical scales; they consist of a key and a mode - minor or major
+# For better readability the minor scales will be written in lower case
+def format_key(row):
+    if row['mode'] == 0:  # moll
+        return row['key'].lower()
+    else:  # dur
+        return row['key']
+
+# Apply the transformation
+df['key_mode'] = df.apply(format_key, axis=1)
+
+mode_mapping = {0: '-moll', 1: '-dur'}
+
