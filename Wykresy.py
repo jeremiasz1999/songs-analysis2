e2e0102deeb5c0688669d9eaef2c0883e8cc9581
Piotr-Przypasniak <p.przypasniak.550@studms.ug.edu.pl>
2025-02-03 17:26:55 +0100

    Added corelation matrix

--- Wykresy.py
+++ Wykresy.py
@@ -1,11 +1,12 @@
 from song_popularity_analysis import df
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import seaborn as sns
 
 
 # Dividing parameters into numerical and categorical
 cat = df[["key","mode", "time_signature", "key_mode"]].copy()
-num = df.drop(["key","mode", "time_signature"], axis = 1)
+num = df.drop(["key","mode", "time_signature", "valence", "loudness"], axis = 1)
 
 # ///////////////////////////////// Wykresy zmiennych ciągłych \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 # Tworzymy wykres o rozmiarze (30, 30) i jedną oś (ax)
@@ -27,4 +28,18 @@ ax1.legend(handles=[moll_patch, dur_patch], loc='upper right')
 #-------------------------------------------------------------------------------------------------
 cat["time_signature"].value_counts().plot(kind='bar', ax = ax2, title = "Time signature")
 cat["mode"].value_counts().plot(kind='bar', ax = ax3, title = "audio mode")
+plt.show()
+
+
+# //////////////////////////////\//////Macierz korelacji\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+correlation_matrix=df[['popularity', 'duration_ms', 'acousticness',
+                       'danceability', 'energy', 'instrumentalness',
+                       'liveness', 'loudness', 'speechiness', 'tempo',
+                       'valence']].corr()
+
+plt.figure(figsize=(12,10))
+ax = sns.heatmap(correlation_matrix, annot=True, cmap='RdBu_r')
+ax.xaxis.tick_top()
+plt.xticks(rotation=90)
 plt.show()
